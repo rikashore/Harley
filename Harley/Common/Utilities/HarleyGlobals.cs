729c0a70057c9a4eb133a79f942c4a3fecdf216c
Aarav Navani <aarav.navani@outlook.com>
2021-05-30 22:16:47 +0530

    Added globals

---
+++ Harley/Common/Utilities/HarleyGlobals.cs
@@ -0,0 +1,13 @@
+﻿using Discord;
+
+namespace Harley.Common.Utilities
+{
+    public static class HarleyGlobals
+    {
+        public static Color HarleyColor = new Color(166, 36, 41);
+        public static ulong OwnerId = 714821968825810945;
+        public static string LibraryRepo = "https://github.com/Discord-Net-Labs/Discord.Net-Labs";
+        public static string LibraryRepoMarkdown =
+            "[Discord.Net-Labs](https://github.com/Discord-Net-Labs/Discord.Net-Labs)";
+    }
+}
---
+++ Harley/Common/Utilities/InfoEmbeds.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Harley.Common.Extensions;
+
+namespace Harley.Common.Utilities
+{
+    public static class InfoEmbeds
+    {
+        public static readonly Embed MainInfoEmbed = new EmbedBuilder()
+            .WithTitle("Harley, the bot that uses Labs")
+            .WithDescription("A bot built to showcase Discord.Net-Labs")
+            .WithHarleyColor()
+            .AddField("Library", HarleyGlobals.LibraryRepoMarkdown)
+            .AddField("Made by", "shift-eleven#7304")
+            .Build();
+
+        public static readonly Embed NameInfoEmbed = new EmbedBuilder()
+            .WithTitle("Why the name Harley?")
+            .WithDescription("Well because this dude named Quin made labs, and the first thing in my mind was Harley Quinn, so there you go")
+            .WithHarleyColor()
+            .Build();
+    }
+}
