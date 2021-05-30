aa06685a4114a969aa6ce6e6d0baacf27a568723
Aarav Navani <aarav.navani@outlook.com>
2021-05-30 22:17:59 +0530

    Added info module

---
+++ Harley/Modules/Commands/InfoModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Harley.Common.Utilities;
+
+namespace Harley.Modules.Commands
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("info")]
+        public async Task InfoAsync()
+        {
+            var infoButton = new ButtonBuilder()
+                .WithLabel("Info")
+                .WithStyle(ButtonStyle.Primary)
+                .WithCustomId("main_info");
+
+            var nameButton = new ButtonBuilder()
+                .WithLabel("The name?")
+                .WithStyle(ButtonStyle.Secondary)
+                .WithCustomId("why_name");
+
+            var componentBuilder = new ComponentBuilder()
+                .WithButton(infoButton)
+                .WithButton(nameButton)
+                .Build();
+
+            await ReplyAsync(embed: InfoEmbeds.MainInfoEmbed, component: componentBuilder);
+        }
+    }
+}
