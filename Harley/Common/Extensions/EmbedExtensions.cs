000cda33d9f2c5a8545b41df9a5e4912719241ce
Aarav Navani <aarav.navani@outlook.com>
2021-05-30 22:17:00 +0530

    Added embed extensions

---
+++ Harley/Common/Extensions/EmbedExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Discord;
+using Harley.Common.Utilities;
+
+namespace Harley.Common.Extensions
+{
+    public static class EmbedExtensions
+    {
+        public static EmbedBuilder WithHarleyColor(this EmbedBuilder builder) 
+            => builder.WithColor(HarleyGlobals.HarleyColor);
+    }
+}
