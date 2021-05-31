af9d95c0d6a374a551745344995fc8a2636c066e
Aarav Navani <aarav.navani@outlook.com>
2021-05-31 13:16:08 +0530

    Added fun module

---
+++ Harley/Modules/Commands/FunModule.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Harley.Modules.Commands
+{
+    public class FunModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("rps"), Summary("Play Rock Paper Scissors with Harley")]
+        public async Task RpsAsync()
+        {
+            var rockButton = new ButtonBuilder()
+                .WithStyle(ButtonStyle.Primary)
+                .WithLabel("Rock")
+                .WithCustomId("rock_button");
+            
+            var paperButton = new ButtonBuilder()
+                .WithStyle(ButtonStyle.Success)
+                .WithLabel("Paper")
+                .WithCustomId("paper_button");
+            
+            var scissorButton = new ButtonBuilder()
+                .WithStyle(ButtonStyle.Secondary)
+                .WithLabel("Scissors")
+                .WithCustomId("scissor_button");
+
+            var component = new ComponentBuilder()
+                .WithButton(rockButton)
+                .WithButton(paperButton)
+                .WithButton(scissorButton)
+                .Build();
+
+            await ReplyAsync("Choose!", component: component);
+        } 
+    }
+}
