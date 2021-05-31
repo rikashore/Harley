446689fb1e277fddd86a94125f4e6e246e146779
Aarav Navani <aarav.navani@outlook.com>
2021-05-31 13:16:44 +0530

    Added service collection extensions

---
+++ Harley/Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Reflection;
+using Harley.Common.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Harley.Common.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHarleyServices(this IServiceCollection services)
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(x => x.GetCustomAttribute<HarleyService>() != null);
+
+            foreach (var type in types)
+                services.AddSingleton(type);
+
+            return services;
+        }
+    }
+}
